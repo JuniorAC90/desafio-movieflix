f5f6fd72dc28ab9d9a4c429c50622a6c73b5e227
Aloizio Junior <66444799+JuniorAC90@users.noreply.github.com>
2025-12-05 02:01:30 -0300

    Add order and sort in findByGenre

--- src/main/java/com/devsuperior/movieflix/controllers/MovieController.java
+++ src/main/java/com/devsuperior/movieflix/controllers/MovieController.java
@@ -1,14 +1,16 @@
 package com.devsuperior.movieflix.controllers;
 
+import com.devsuperior.movieflix.dto.MovieCardDTO;
 import com.devsuperior.movieflix.dto.MovieDetailsDTO;
 import com.devsuperior.movieflix.services.MovieService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/movies")
@@ -22,5 +24,13 @@ public class MovieController {
     public ResponseEntity<MovieDetailsDTO> findById(@PathVariable Long id) {
         MovieDetailsDTO dto = service.findById(id);
         return ResponseEntity.ok().body(dto);
+    }
+
+    @PreAuthorize("hasAnyRole('VISITOR', 'MEMBER')")
+    @GetMapping
+    public ResponseEntity<Page<MovieCardDTO>> findByGenre(@RequestParam(required = false, defaultValue = "0") Long genreId, Pageable pageable) {
+        Pageable pageRequest = PageRequest.of(pageable.getPageNumber(), pageable.getPageSize(), Sort.by("title"));
+        Page<MovieCardDTO> list = service.findByGenre(genreId, pageRequest);
+        return ResponseEntity.ok().body(list);
     }
 }
--- src/main/java/com/devsuperior/movieflix/services/MovieService.java
+++ src/main/java/com/devsuperior/movieflix/services/MovieService.java
@@ -1,5 +1,6 @@
 package com.devsuperior.movieflix.services;
 
+import com.devsuperior.movieflix.dto.MovieCardDTO;
 import com.devsuperior.movieflix.dto.MovieDetailsDTO;
 import com.devsuperior.movieflix.entities.Genre;
 import com.devsuperior.movieflix.entities.Movie;
@@ -7,6 +8,8 @@ import com.devsuperior.movieflix.repositories.GenreRepository;
 import com.devsuperior.movieflix.repositories.MovieRepository;
 import com.devsuperior.movieflix.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -25,10 +28,19 @@ public class MovieService {
     @Transactional(readOnly = true)
     public MovieDetailsDTO findById(Long id) {
         Movie entity = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
-        return copyEntityToDto(entity);
+        return copyEntityToMovieDetailsDto(entity);
     }
 
-    private MovieDetailsDTO copyEntityToDto(Movie entity) {
+    @Transactional(readOnly = true)
+    public Page<MovieCardDTO> findByGenre(Long genreId, Pageable pageable) {
+        if (genreId == 0) {
+            return repository.findAll(pageable).map(this::copyEntityToMovieCardDto);
+        } else {
+            return repository.findByGenreId(genreId, pageable).map(this::copyEntityToMovieCardDto);
+        }
+    }
+
+    private MovieDetailsDTO copyEntityToMovieDetailsDto(Movie entity) {
         MovieDetailsDTO dto = new MovieDetailsDTO();
         dto.setId(entity.getId());
         dto.setTitle(entity.getTitle());
@@ -39,6 +51,17 @@ public class MovieService {
 
         Genre genre = genreRepository.findById(entity.getId()).orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
         dto.setGenre(genreService.copyEntityToDto(genre));
+
+        return dto;
+    }
+
+    private MovieCardDTO copyEntityToMovieCardDto(Movie entity) {
+        MovieCardDTO dto = new MovieCardDTO();
+        dto.setId(entity.getId());
+        dto.setTitle(entity.getTitle());
+        dto.setSubTitle(entity.getSubTitle());
+        dto.setYear(entity.getYear());
+        dto.setImgUrl(entity.getImgUrl());
 
         return dto;
     }
